c81eda5a37f69a64052e10f6d569373d8d8eef69
Bektur <bekturmashaev@gmail.com>
2023-12-11 19:06:20 +0600

    New info

--- app/src/main/java/com/example/composeproject/CarItem.kt
+++ app/src/main/java/com/example/composeproject/CarItem.kt
@@ -1,20 +1,26 @@
 package com.example.composeproject
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -34,7 +40,7 @@ fun CarItem(carModel: CarModel) {
             ) {
                 Text(
                     text = carModel.carName,
-                    fontSize = 18.sp,
+                    fontSize = 15.sp,
                     color = Color.Black,
                     fontFamily = FontFamily.Monospace,
                     fontWeight = FontWeight.Bold
@@ -47,7 +53,7 @@ fun CarItem(carModel: CarModel) {
                 )
                 Text(
                     text = carModel.carSpeed,
-                    fontSize = 15.sp,
+                    fontSize = 12.sp,
                     color = Color.Black,
                     fontFamily = FontFamily.Monospace,
                     fontWeight = FontWeight.Bold
@@ -55,8 +61,9 @@ fun CarItem(carModel: CarModel) {
             }
             AsyncImage(model = carModel.carImage,
                 contentDescription = null,
-                modifier = Modifier.height(80.dp).clip(shape = RoundedCornerShape(10.dp)),
-                contentScale = ContentScale.Crop)
+                modifier = Modifier.width(150.dp).height(80.dp).clip(shape = RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop,
+                )
         }
     }
 }
